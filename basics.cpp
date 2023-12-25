23fcaf9312a2b7f634cc9e4c544b9b733c3d438d
Benjamin Reifler <ben.reifler@gmail.com>
2023-12-25 13:58:34 -0600

    Minor update (new naming convention for non-`const` `static` variables).

--- basics.cpp
+++ basics.cpp
@@ -2,30 +2,30 @@
 #include <random>
 #include <thread>
 
-static std::mt19937_64 RandomEngine(std::chrono::system_clock::now().
+static std::mt19937_64 _randomEngine(std::chrono::system_clock::now().
     time_since_epoch().count()^std::hash<std::thread::id>{}(std::this_thread::
     get_id()));
 
 double paz::randn()
 {
     std::normal_distribution<double> dis(0., 1.);
-    return dis(RandomEngine);
+    return dis(_randomEngine);
 }
 
 int paz::randi(int a, int b)
 {
     std::uniform_int_distribution<int> dis(a, b);
-    return dis(RandomEngine);
+    return dis(_randomEngine);
 }
 
 double paz::uniform()
 {
     std::uniform_real_distribution<double> dis(0., 1.);
-    return dis(RandomEngine);
+    return dis(_randomEngine);
 }
 
 double paz::uniform(double a, double b)
 {
     std::uniform_real_distribution<double> dis(a, b);
-    return dis(RandomEngine);
+    return dis(_randomEngine);
 }
